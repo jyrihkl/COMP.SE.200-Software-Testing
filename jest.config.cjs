6fab0c7f06c29dab72c355175e00cfece5a244c7
Jyri Hakala <jyri.hakala@tuni.fi>
2023-12-08 19:36:10 +0200

    Change jest to only collect coverage from src

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   coverageReporters: ['lcov', 'text'],
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.js'],
+  collectCoverageFrom: ['src/*.js'],
   coveragePathIgnorePatterns: ['/node_modules/', '/src/.internal/'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
