cc9c4ba71f1295b7b19ace3133632a4f91fb935c
Jyri Hakala <jyri.hakala@tuni.fi>
2023-12-08 19:37:11 +0200

    Add tests for some toNumber edge cases with objects

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -64,5 +64,67 @@ describe('toNumber', () => {
   it('should return 0 for an empty string', () => {
     expect(toNumber('')).toBe(0);
   });
+
+  it('should handle large numbers without overflow', () => {
+    const largeNumber = '1e1000';
+    expect(toNumber(largeNumber)).toBe(Infinity);
+  });
+
+  it('should handle small (subnormal) numbers without underflow', () => {
+    const smallNumber = '1e-1000';
+    expect(toNumber(smallNumber)).toBe(0);
+  });
+
+  it('should handle objects with a valid valueOf method', () => {
+    const objectWithValidValueOf = {
+      valueOf: function () {
+        return 42;
+      },
+    };
+    expect(toNumber(objectWithValidValueOf)).toBe(42);
+  });
+
+  it('should handle objects with an invalid valueOf method', () => {
+    const objectWithInvalidValueOf = {
+      valueOf: function () {
+        return 'not a number';
+      },
+    };
+    expect(toNumber(objectWithInvalidValueOf)).toBe(NaN);
+  });
+
+  it('should return NaN for an object with NaN value', () => {
+    const objectWithNaN = {
+      valueOf: function () {
+        return NaN;
+      },
+    };
+    expect(toNumber(objectWithNaN)).toBe(NaN);
+  });
+
+  it('should handle objects with valueOf method returning 0', () => {
+    const objectWithZeroValueOf = {
+      valueOf: function () {
+        return 0;
+      },
+    };
+    expect(toNumber(objectWithZeroValueOf)).toBe(0);
+  });
+  
+  it('should handle objects with valueOf method returning non-zero', () => {
+    const objectWithNonZeroValueOf = {
+      valueOf: function () {
+        return 42;
+      },
+    };
+    expect(toNumber(objectWithNonZeroValueOf)).toBe(42);
+  });
+
+  it('should handle objects with valueOf not being a function', () => {
+    const objectWithNonFunctionValueOf = {
+      valueOf: 12,
+    };
+    expect(toNumber(objectWithNonFunctionValueOf)).toBe(NaN);
+  });
 });
 
